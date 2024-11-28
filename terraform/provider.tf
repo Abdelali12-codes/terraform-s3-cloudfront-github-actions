d57a15dce47921b1b0427d5dc6fcee0dbadfa79b
Abdelali12-codes <jadelmoulaa3@gmail.com>
2024-11-28 20:28:35 +0100

    debugg workspace

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,7 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "terraform-state-shared-bucket" # Replace with your bucket name
     key            = "terraform-state/terraform.tfstate" # Customize this key structure if needed
-    workspace_key_prefix = "states"
     region         = "us-east-1" # Replace with your region
     dynamodb_table = "arn:aws:dynamodb:us-east-1:080266302756:table/terraform-state-lock" # Replace with your DynamoDB table name
     encrypt        = true
