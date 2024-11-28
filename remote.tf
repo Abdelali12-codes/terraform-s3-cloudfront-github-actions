423edb4fbb0e405d0b44a82b7346ce2d4377764d
Abdelali12-codes <jadelmoulaa3@gmail.com>
2024-11-28 19:02:20 +0100

    update workflow

--- remote.tf
+++ remote.tf
@@ -30,12 +30,6 @@ resource "aws_dynamodb_table" "terraform_lock" {
 
 
 # aws s3 cross account access
-
-resource "aws_s3_bucket_policy" "s3_cross_account_access" {
-  bucket = aws_s3_bucket.terraform_state.id
-  policy = data.aws_iam_policy_document.s3_cross_account_access.json
-}
-
 data "aws_iam_policy_document" "s3_cross_account_access" {
   statement {
     principals {
@@ -48,7 +42,8 @@ data "aws_iam_policy_document" "s3_cross_account_access" {
        "s3:GetObject",
         "s3:PutObject",
         "s3:DeleteObject",
-        "s3:ListBucket"
+        "s3:ListBucket",
+        "s3:HeadBucket"
     ]
 
     resources = [
@@ -56,6 +51,12 @@ data "aws_iam_policy_document" "s3_cross_account_access" {
       "${aws_s3_bucket.terraform_state.arn}/*",
     ]
   }
+}
+
+resource "aws_s3_bucket_policy" "s3_cross_account_access" {
+  bucket = aws_s3_bucket.terraform_state.id
+  policy = data.aws_iam_policy_document.s3_cross_account_access.json
+  provider =  aws.account1
 }
 
 
@@ -85,5 +86,6 @@ data "aws_iam_policy_document" "dynamodb_cross_account_access" {
 resource "aws_dynamodb_resource_policy" "dynamodb_cross_account_access" {
   resource_arn = aws_dynamodb_table.terraform_lock.arn
   policy       = data.aws_iam_policy_document.dynamodb_cross_account_access.json
+  provider = aws.account1
 }
 
